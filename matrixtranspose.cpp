713d399aef4e1766b1924942085856878c94ce44
Shreyansh Ghanekar <shreyansh0611@gmail.com>
2026-07-13 19:39:46 +0530

    abc

---
+++ matrixtranspose.cpp
@@ -0,0 +1,21 @@
+//rotate matrix by 90 degree
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int n=3 ; 
+    int m=3;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<matrix[i][j];
+        }
+        cout<<endl;
+    }
+    //now transpose
+
+    
+
+}
---
+++ setmatrix_zeros.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*Input: matrix=[[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Explanation: Since matrix[2][2]=0.Therfore the 2nd column and 2nd row wil be set to 0.*/
+
+int main(){
+int 
+}
